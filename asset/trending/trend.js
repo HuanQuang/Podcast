542772921f3768056410d73971b11352e4de8add
Trần Quang Huấn <hyugalaputa@gmail.com>
2022-08-09 12:08:00 +0700

    thích gì ghi nấy :))

--- asset/trending/trend.js
+++ asset/trending/trend.js
@@ -54,151 +54,176 @@ const trendPage = {
             name: '3107LofiVersion',
             path: './Music/3107LofiVersion-WnNauDuongg-6943942.mp3',
             image: './img/3107.jpg',
-            author: 'W, n, Nâu, Duong'
+            author: 'W, n, Nâu, Duong',
+            duration: '2:22'
         },
         {
             name: 'Anh mệt rồi',
             path: './Music/AnhMetRoiLofiVersion-AnhQuanIdolFreakD-6811840.mp3',
             image: './img/anh mệt rồi.jpg',
-            author: 'Anh Quân Idol, FreakD'
+            author: 'Anh Quân Idol, FreakD',
+            duration: '4:22'
         },
         {
             name: 'Bỏ lỡ nhau rồi',
             path: './Music/BoLoNhauRoiLofiVersion-HaiNamFreakD-6930872.mp3',
             image: './img/bỏ lỡ nhau rồi.jpg',
-            author: 'Hải Nam, FreakD'
+            author: 'Hải Nam, FreakD',
+            duration: '5:07'
         },
         {
             name: 'Chẳng ai yêu mãi một người',
             path: './Music/ChangAiYeuMaiMotNguoiLofiVersion-NB3HoaiBaoDongDangFreakD-6861156.mp3',
             image: './img/Chẳng ai yêu mãi 1 người.jpg',
-            author: 'NB3 Hoài Bảo, Đồng Đăng, FreakD'
+            author: 'NB3 Hoài Bảo, Đồng Đăng, FreakD',
+            duration: '5:03'
         },
         {
             name: 'Chỉ muốn bên em lúc này',
             path: './Music/ChiMuonBecircnEmLuacutecNagraveyFreakDLofiVersion-HuyVac-6940119.mp3',
             image: './img/Chỉ muốn bên em lúc này.jpg',
-            author: 'Huy Vạc, FreakD'
+            author: 'Huy Vạc, FreakD',
+            duration:'3:47'
         },
         {
             name: 'Chỉ vì quá thương em',
             path: './Music/ChiViQuaThuongEmLofiVersion-HaiNamFreakD-6861154.mp3',
             image: './img/chỉ vì quá thương em.jpg',
-            author: 'Hải Nam, FreakD'
+            author: 'Hải Nam, FreakD',
+            duration:'5:12'
         },
         {
             name: 'Chúng ta sau này',
             path: './Music/ChungTaSauNayLofiVer-TRIFreakD-6953708.mp3',
             image: './img/Chúng ta sau này.jpg',
-            author: 'TRI, FreakD'
+            author: 'TRI, FreakD',
+            duration:'2:43'
         },
         {
             name: 'Đừng xem ai đó là cả thế giới',
             path: './Music/DungXemAiDoLaCaTheGioiLofiVersion-ReddyHuuDuy-6281321.mp3',
             image: './img/đừng xem ai đó là cả thế giới.jpg',
-            author: 'Reddy'
+            author: 'Reddy',
+            duration:'4:16'
         },
         {
             name: 'Đường tôi chở em về',
             path: './Music/DuongToiChoEmVeLofiVersion-buitruonglinhFreakD-7025960_hq.mp3',
             image: './img/2.png',
-            author: 'Bùi Trường Linh, FreakD'
+            author: 'Bùi Trường Linh, FreakD',
+            duration:'4:38'
         },
         {
             name: 'Hẹn Yêu',
             path: './Music/HenYeuLofiVersion-DuyZunoCaoTri-6879088.mp3',
             image: './img/hẹn yêu.jpg',
-            author: 'Duy Zuno'
+            author: 'Duy Zuno',
+            duration:'3:50'
         },
         {
             name: 'Kẻ điên tin vào tình yêu',
             path: './Music/KeDienTinVaoTinhYeuLofiVersion-LilZpoetFreakD-6861163.mp3',
             image: './img/me.jpg',
-            author: 'LilZpoet, FreakD'
+            author: 'LilZpoet, FreakD',
+            duration:'3:45'
         },
         {
             name: 'Kẻ mộng mơ',
             path: './Music/KeMongMoLofiVer-ReddyFreakD-6929548.mp3',
             image: './img/kẻ mộng mơ.jpg',
-            author: 'Reddy, FreakD'
+            author: 'Reddy, FreakD',
+            duration:'5:29'
         },
         {
             name: 'Lạ lùng',
             path: './Music/LaLungLofiVersion-Vu-6181036.mp3',
             image: './img/lạ lùng.jpg',
-            author: 'VŨ'
+            author: 'VŨ',
+            duration:'4:29'
         },
         {
             name: 'Liệu giờ',
             path: './Music/LieuGioEkidLofiVersion-2TVennVanEkid-6952139.mp3',
             image: './img/liệu giờ.jpg',
-            author: '2TVennVanEkid'
+            author: '2TVennVanEkid',
+            duration:'4:33'
         },
         {
             name: 'Mình anh nơi này',
             path: './Music/MinhAnhNoiNayLofiVersion-NITSingFreakD-7022891.mp3',
             image: './img/me.jpg',
-            author: 'NITSing, FreakD'
+            author: 'NITSing, FreakD',
+            duration:'4:37'
         },
         {
             name: 'Những gì anh nói',
             path: './Music/NhungGiAnhNoiLofiVersionByHawys-Bozitt-6946199.mp3',
             image: './img/me.jpg',
-            author: 'Bozitt'
+            author: 'Bozitt',
+            duration:'5:36'
         },
         {
             name: 'Nợ ai đó lời xin lỗi',
             path: './Music/NoAiDoLoiXinLoiLofiVersion-BozittCaoTri-6879092.mp3',
             image: './img/Nợ ai đó lời xin lỗi.jpg',
-            author: 'Bozitt, CaoTri'
+            author: 'Bozitt, CaoTri',
+            duration:'5:46'
         },
         {
             name: 'Nước mắt em lau bằng tình yêu mới',
             path: './Music/NuocMatEmLauBangTinhYeuMoiCM1XLofiRemix-DaLABTocTien-6098438.mp3',
             image: './img/Nước mắt e lau bằng tình yêu mới.jpg',
-            author: 'DaLAB, TocTien'
+            author: 'DaLAB, TocTien',
+            duration:'4:27'
         },
         {
             name: 'Phận duyên lỡ làng',
             path: './Music/Phận duyên lỡ làng - Phát Huy T4, Trungzg, Kprox.mp3',
             image: './img/phận duyên lỡ làng.jpg',
-            author: 'Phát Huy T4, Trungzg, Kprox'
+            author: 'Phát Huy T4, Trungzg, Kprox',
+            duration:'3:45'
         },
         {
             name: 'Răng khôn',
             path: './Music/RangKhonLofiVersion-PhiPhuongAnhTheFace-7026147.mp3',
             image: './img/răng khôn.jpg',
-            author: 'Phí Phương Anh'
+            author: 'Phí Phương Anh',
+            duration:'3:16'
         },
         {
             name: 'Sài gòn đau lòng quá',
             path: './Music/SaiGonDauLongQuaLofiVersion-HuaKimTuyenSIVAN-7006455.mp3',
             image: './img/sài gonnf đau lòng quá.jpg',
-            author: 'HuaKimTuyen, SIVAN'
+            author: 'HuaKimTuyen, SIVAN',
+            duration:'5:15'
         },
         {
             name: 'Sài gòn hôm nay mưa',
             path: './Music/SaiGonHomNayMuaLofiRainVersion-JSOLHoangDuyen-7034696.mp3',
             image: './img/sài gòn hôm nay mưa.jpg',
-            author: 'JSOL, HoangDuyen'
+            author: 'JSOL, HoangDuyen',
+            duration:'4:22'
         },
         {
             name: 'Sợ rằng em biết anh còn yêu em',
             path: './Music/SoRangEmBietAnhConYeuEmLofiVersion-JuunDangDungFreakD-6248597_hq.mp3',
             image: './img/sợ rằng em biết anh còn yêu em.jpg',
-            author: 'Juun Đăng Dũng, FreakD'
+            author: 'Juun Đăng Dũng, FreakD',
+            duration:'4:50'
         },
         {
             name: 'Thì thôi',
             path: './Music/ThiThoiLofiVer-ReddyFreakD-6929545.mp3',
             image: './img/vài giây nữa thôi.jpg',
-            author: 'Reddy, FreakD'
+            author: 'Reddy, FreakD',
+            duration:'5:48'
         },
         {
             name: 'Vài giây nữa thôi',
             path: './Music/VaiGiayNuaThoiLofiVer-ReddyFreakD-6929544.mp3',
             image: './img/vài giây nữa thôi.jpg',
-            author: 'Reddy, FreakD'
+            author: 'Reddy, FreakD',
+            duration:'4:43'
         },
     ],
     render: function(){
@@ -225,7 +250,10 @@ const trendPage = {
                     <h3 class="playList--item__name">${song.name}</h3>
                     <p class="playList--item__author">${song.author}</p>
                 </div>
-                <div class="time">5:06</div>
+                <div class="time">
+                <span class="timeleft">00:00</span> /
+                <span class="timeright">${song.duration}</span>
+                </div>
                 <div class="playList--item--option">
                     <i class="fa-solid fa-ellipsis"></i>
                 </div>           
@@ -244,7 +272,7 @@ const trendPage = {
                     <h3 class="playList--item__name">${song.name}</h3>
                     <p class="playList--item__author">${song.author}</p>
                 </div>
-                <div class="time">5:06</div>
+                <div class="time">${song.duration}</div>
                 <div class="playList--item--option">
                     <i class="fa-solid fa-ellipsis"></i>
                 </div>      
@@ -264,7 +292,7 @@ const trendPage = {
                     <h3 class="playList--item__name">${song.name}</h3>
                     <p class="playList--item__author">${song.author}</p>
                 </div>
-                <div class="time">5:06</div>
+                <div class="time">${song.duration}</div>
                 <div class="playList--item--option">
                     <i class="fa-solid fa-ellipsis"></i>
                 </div>           
@@ -299,12 +327,17 @@ const trendPage = {
             if(audio.duration) {
                 const percentProgress = (audio.currentTime/audio.duration)*100;
                 progressPlay.value = percentProgress;
+                    $('.activeCurrentSong .timeleft').innerHTML = trendPage.convertTime(audio.currentTime);                
             }
         };
         // khi tua nhạc thì position phát nhạc sẽ thay đổi
         progressPlay.onchange = function(e){
             const seekTime = audio.duration / 100 * e.target.value;
             audio.currentTime = seekTime;
+            $('.pause--icon').classList.remove('hide');
+            $('.play--icon').classList.add('hide');
+            $('.cd__thumb--img').classList.add('cd--rotate');
+            $('.cd__thumb--img').classList.remove('cd-pause');
             audio.play();
            
         };
@@ -348,6 +381,7 @@ const trendPage = {
             trendPage.scrollActiveSong();
 
         };
+        
         // hành vi click on song
         $('.list--render').onclick = function(e){
             if(e.target.closest('.playList--item')){
@@ -366,6 +400,17 @@ const trendPage = {
             }
             
         }
+    },
+    convertTime: function (time) {
+        var mins = Math.floor(time / 60);
+        var secs = Math.floor(time % 60);
+        if (mins < 10) {
+            mins = '0' + mins;
+        }
+        if (secs < 10) {
+            secs = '0' + secs;
+        }
+        return `${mins}:${secs}`;
     },
     defineProperties: function(){
         const set = Number($('.control__btn.active').dataset.index);
